214b743cfe7fb1d09ceddfe257df7300ee0f4e15
samniu <samniu.ca@gmail.com>
2025-02-22 13:27:55 -0800

    completed tasks folded

--- lib/widgets/todo_list.dart
+++ lib/widgets/todo_list.dart
@@ -3,53 +3,78 @@ import '../models/todo.dart';
 import 'todo_item.dart';
 import 'package:get/get.dart';
 
-
-class TodoList extends StatelessWidget {
+class TodoList extends StatefulWidget {
   final List<Todo> todos;
   final Function(String) onToggle;
   final Function(String) onToggleFavorite;
-  final Function(Todo) onTaskTap;  // 更改为 onTaskTap
+  final Function(Todo) onTaskTap;
 
   const TodoList({
     super.key,
     required this.todos,
     required this.onToggle,
     required this.onToggleFavorite,
-    required this.onTaskTap,  // 更新参数名
+    required this.onTaskTap,
   });
 
   @override
+  State<TodoList> createState() => _TodoListState();
+}
+
+class _TodoListState extends State<TodoList> {
+  bool _isCompletedExpanded = true;
+
+  @override
   Widget build(BuildContext context) {
-    final incompleteTodos = todos.where((todo) => !todo.isCompleted).toList();
-    final completedTodos = todos.where((todo) => todo.isCompleted).toList();
+    final incompleteTodos = widget.todos.where((todo) => !todo.isCompleted).toList();
+    final completedTodos = widget.todos.where((todo) => todo.isCompleted).toList();
 
     return ListView(
       padding: const EdgeInsets.all(16),
       children: [
         ...incompleteTodos.map((todo) => TodoItem(
               todo: todo,
-              onToggle: onToggle,
-              onToggleFavorite: onToggleFavorite,
-              onTap: onTaskTap,  // 更新参数名
+              onToggle: widget.onToggle,
+              onToggleFavorite: widget.onToggleFavorite,
+              onTap: widget.onTaskTap,
             )),
         if (completedTodos.isNotEmpty) ...[
-           Padding(
-            padding: EdgeInsets.symmetric(vertical: 16.0),
-            child: Text(
-              'completed'.tr,
-              style: TextStyle(
-                color: Colors.white70,
-                fontSize: 16,
-                fontWeight: FontWeight.bold,
+          InkWell(
+            onTap: () {
+              setState(() {
+                _isCompletedExpanded = !_isCompletedExpanded;
+              });
+            },
+            child: Padding(
+              padding: const EdgeInsets.symmetric(vertical: 16.0),
+              child: Row(
+                children: [
+                  Icon(
+                    _isCompletedExpanded 
+                        ? Icons.keyboard_arrow_down
+                        : Icons.keyboard_arrow_right,
+                    color: Colors.white70,
+                  ),
+                  const SizedBox(width: 8),
+                  Text(
+                    'completed'.tr + ' (${completedTodos.length})',
+                    style: const TextStyle(
+                      color: Colors.white70,
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ],
               ),
             ),
           ),
-          ...completedTodos.map((todo) => TodoItem(
-                todo: todo,
-                onToggle: onToggle,
-                onToggleFavorite: onToggleFavorite,
-                onTap: onTaskTap,  // 更新参数名
-              )),
+          if (_isCompletedExpanded)
+            ...completedTodos.map((todo) => TodoItem(
+                  todo: todo,
+                  onToggle: widget.onToggle,
+                  onToggleFavorite: widget.onToggleFavorite,
+                  onTap: widget.onTaskTap,
+                )),
         ],
       ],
     );
